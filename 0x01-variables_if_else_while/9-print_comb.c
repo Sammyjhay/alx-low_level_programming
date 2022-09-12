30ec7aecf5d80ea4f7996802bf03d9e4986a69ca
Sammyjhay <samueladeyanju9@gmail.com>
2022-09-12 16:18:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints single digit numbers in a new line
+ * and also adds a comma except after the last single digit
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			putchar('\n');
+			return (0);
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
