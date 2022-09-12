41edbd3d194e9a88533cceb306add8e114b3b662
Sammyjhay <samueladeyanju9@gmail.com>
2022-09-12 09:33:18 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints single numbers
+ * from 0 on a new line
+ * using only putchar twice
+ * remember to use for loop, and increament
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
