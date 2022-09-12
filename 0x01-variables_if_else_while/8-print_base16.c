2abb305c5b46d60e89d7f209c5d1cd00063bdd1d
Sammyjhay <samueladeyanju9@gmail.com>
2022-09-12 15:29:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints nuumbers in hexadecimal
+ * remember to print all numbers in new line and use putchar twice
+ * print 0-9 first then a-f
+ * use for loop
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n;
+	char x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
