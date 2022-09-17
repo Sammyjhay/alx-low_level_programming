b397c0bd2e068657b40a1bc44e24cfcc6d3dd434
Sammyjhay <samueladeyanju9@gmail.com>
2022-09-17 02:40:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - a functions that multiplies two arguments
+ * @a: a parameter that will be passed into the function
+ * @b: a parameter that will be passed into the function
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
